4a6bdeb2ea85d795b5ab50be4f902c4d22841a0a
Jenner <jenner9212@gmail.com>
2023-09-09 01:51:52 +0900

    Create lv1_최소직사각형.cpp

---
+++ programmers/c++/완전탐색/lv1_최소직사각형.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0, h = 0, w = 0;
+
+    for(int i = 0; i < sizes.size(); i++){
+        if(sizes[i][0] > sizes[i][1]){
+            h = max(h, sizes[i][0]);
+            w = max(w, sizes[i][1]);
+        }
+        else{
+            h = max(h, sizes[i][1]);
+            w = max(w, sizes[i][0]);
+        }
+    }
+    answer = h * w;
+
+    return answer;
+}
